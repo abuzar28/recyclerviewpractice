89e9e7092c41fc9d46e739fddabd5a6adfdb9742
abuzar.bit <abuzarrehan95@gmail.com>
2021-11-24 10:53:39 +0500

    my adpter class

--- app/src/main/java/com/example/recylerview/MainActivity.java
+++ app/src/main/java/com/example/recylerview/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.recylerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    String name[]={"facebook","instagram"}
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list);
+
     }
+class myadapter extends ArrayAdapter<String>{
+
+        myadapter(){
+            sdfhaskjflawsfnjbsdbf
+                    xfhasadhfl.sdnbv
+                            avnkvdsbxflks,dmvjbn
+                            gfjafdfkgjgkfdjmvn
+
+        }
+
 }
+}
+
+
+//   ArrayList<String> todo=new ArrayList<String>();
+//            todo.add("fajar");
+//            todo.add("zuhar");
+//            todo.add("asar");
+//            todo.add("maghrib");
+//            todo.add("esha");
+//        ArrayAdapter<String> prayer=new ArrayAdapter<String>(this,R.layout.listarrayadp,R.id.textView2,todo);
+//        listView.setAdapter(prayer);
+
+
+
+
+
